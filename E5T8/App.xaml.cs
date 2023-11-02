bf971e41fc8bed8dbb07854edb0101b75e32a720
34611 <34611@AJARE>
2023-11-02 20:12:53 +0100

    Agregar archivos de proyecto.

---
+++ E5T8/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace E5T8
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ E5T8/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace E5T8
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
